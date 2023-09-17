12523f82fac1b0974cf967b6e871acaa772be134
admin <admin@MSI>
2023-09-17 11:15:30 +0700

    Add project files.

---
+++ NguyenTanBinh_2108110400_K15DCMPM03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NguyenTanBinh_2108110400_K15DCMPM03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
